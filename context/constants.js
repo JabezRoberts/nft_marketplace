0e419707c5c6eb3079bd437ab51f0565ea51a9c3
Jabez Roberts <info@jabezroberts.com>
2022-08-03 17:23:44 -0500

    Final big fix and key lead issue

--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,5 @@
 import market from './NFTMarketplace.json';
 
-export const MarketAddress = '0x65e06a3edBE3cafe792fF3035Ced977Ca3c005C3';
+export const MarketAddress = '0x388622B732726840B9285D1B0f529fa9E9c98f95';
 export const MarketAddressAbi = market.abi;
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ const privateKey = fs.readFileSync('.secret').toString().trim();
 module.exports = {
   networks: {
     goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/fHy1C1uzVUOERhvd4OMPwAygYTzbBwi7',
+      url: 'https://eth-goerli.g.alchemy.com/v2/lgTG9wF-PyHyQ6wfdKVQtvO8ezFISy15',
       accounts: ['1b697ce78baec6a24fe5b279c3dc63c77f0e101df29c1178481dde7c3be7fa3b'],
     },
   },
