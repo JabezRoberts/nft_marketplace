ff7eb9e2cd798d242f9449b2033794f7c95a7020
Jabez Roberts <info@jabezroberts.com>
2022-08-03 04:43:01 -0500

    deploy to vercel then go live on custom domain

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,9 @@ const privateKey = fs.readFileSync('.secret').toString().trim();
 
 module.exports = {
   networks: {
-    hardhat: {
-      chainId: 1337,
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/fHy1C1uzVUOERhvd4OMPwAygYTzbBwi7',
+      accounts: ['1b697ce78baec6a24fe5b279c3dc63c77f0e101df29c1178481dde7c3be7fa3b'],
     },
   },
   solidity: '0.8.4',
