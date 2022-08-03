a6bbd63fd96789100e4f8abe19312830321c7a20
Jabez Roberts <info@jabezroberts.com>
2022-08-03 17:32:38 -0500

    Fix bug issues

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+require('@nomiclabs/hardhat-waffle');
+
+const privateKey = fs.readFileSync('.secret').toString().trim();
+
+module.exports = {
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/lgTG9wF-PyHyQ6wfdKVQtvO8ezFISy15',
+      accounts: ['1b697ce78baec6a24fe5b279c3dc63c77f0e101df29c1178481dde7c3be7fa3b'],
+    },
+  },
+  solidity: '0.8.4',
+};
